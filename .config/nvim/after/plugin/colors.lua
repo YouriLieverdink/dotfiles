2df74b5d950bd1e0b1ae8004eec2ce273cdb8e9a
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-08-09 16:05:34 +0200

    refactor: change color of sign column in nvim

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,3 +1,4 @@
 -- Set background color to transparent
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" });
